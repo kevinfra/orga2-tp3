2d85e682b3e3b1da995d890c5784d7ef9020fceb
Kevin Frach <kevinfra94@gmail.com>
2016-07-03 20:14:48 -0300

    aparentemente funciona, pero cuando explota una tarea medio que wtf

--- entregable/src/game.c
+++ entregable/src/game.c
@@ -17,6 +17,8 @@ int puntajeRojo;
 int puntajeAzul;
 int juegoEstaIniciado = 0;
 int tareasEnJuego[2];
+int vidasAzul;
+int vidasRojas;
 
 void iniciarGame(){
   X_A=20;
@@ -31,6 +33,12 @@ void iniciarGame(){
   tareasEnJuego[1] = 0;
   pintarTarea(X_A,Y_A,0); // Pinta Rojo
   pintarTarea(X_B,Y_B,1); // Pinta Azul
+  vidasAzul = 15;
+  vidasRojas = 15;
+  print_int(vidasRojas, 48, 44, (C_BG_BLACK | C_FG_WHITE));
+  print_int(vidasAzul, 48, 63, (C_BG_BLACK | C_FG_WHITE));
+  print_int(0, 47, 51, (C_BG_RED | C_FG_WHITE));
+  print_int(0, 47, 57, (C_BG_BLUE | C_FG_WHITE));
 }
 
 int juegoIniciado(){
@@ -159,12 +167,16 @@ void sumarTareaLanzada(unsigned int jugador){
 
 void game_lanzar(unsigned int jugador) {
   if(juegoEstaIniciado && tareasLanzadas(jugador) < 5){
-    if(jugador == 1){ // azul
+    if(jugador == 1 && vidasAzul > 0){ // azul
       inicializar_tss(0x12000, X_B, Y_B);
       pintarTarea(X_B, Y_B, 1);
-    }else if(jugador == 0){
+      vidasAzul--;
+      print_int(vidasAzul, 48, 63, (C_BG_BLACK | C_FG_WHITE));
+    }else if(jugador == 0 && vidasRojas > 0){
       inicializar_tss(0x11000, X_A, Y_A);
       pintarTarea(X_A, Y_A, 0);
+      vidasRojas--;
+      print_int(vidasRojas, 48, 44, (C_BG_BLACK | C_FG_WHITE));
     }
     sumarTareaLanzada(jugador);
   }
@@ -179,6 +191,7 @@ void game_donde(unsigned int* pos) {
 
 void game_mapear(int x, int y) {
   unsigned int tareaAMapear = (unsigned int) dameTarea();
+  //breakpoint();
   unsigned int cr3 = rcr3();
   unsigned int dirAMapear = (x + y*80)*4096 + 400000;
   mmu_mapear_pagina(tareaAMapear, cr3, dirAMapear);
@@ -188,30 +201,31 @@ void game_mapear(int x, int y) {
 
 void pintarPuntajeRojo(){
   puntajeRojo++;
-  print_int(puntajeRojo, 48, 51, (C_BG_RED | C_FG_WHITE));
+  print_int(puntajeRojo, 47, 51, (C_BG_RED | C_FG_WHITE));
 }
 
 void pintarPuntajeAzul(){
   puntajeAzul++;
-  print_int(puntajeAzul, 48, 57, (C_BG_BLUE | C_FG_WHITE));
+  print_int(puntajeAzul, 47, 57, (C_BG_BLUE | C_FG_WHITE));
 }
 
 
 void volverDeExcepcion(){
+  //breakpoint();
     short colaDeTareaMuerta = (colaActual - 1) % 3;
     int indiceTareaABorrar;
     switch (colaActual) {
       case 0:
-        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta] -1) % 5;
+        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta]);
         jugadores[colaJugadorB][indiceTareaABorrar].presente = 0;
         tareasEnJuego[1] -= 1;
         break;
       case 1:
-        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta] -1) % 15;
+        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta]);
         jugadores[colaNadie][indiceTareaABorrar].presente = 0;
         break;
       case 2:
-        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta] -1) % 5;
+        indiceTareaABorrar = (siguienteIndiceDeTareaEnCola[colaDeTareaMuerta]);
         jugadores[colaJugadorA][indiceTareaABorrar].presente = 0;
         tareasEnJuego[0] -= 1;
         break;
